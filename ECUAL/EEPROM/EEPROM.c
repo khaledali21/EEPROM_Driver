dd6954623b48ad827fc9ed05cd7bd1c934633336
Khaled Ali <khaled21.ali33@gmail.com>
2022-02-06 03:04:18 +0200

    EOD

--- ECUAL/EEPROM/EEPROM.c
+++ ECUAL/EEPROM/EEPROM.c
@@ -10,12 +10,19 @@
 #include "EEPROM.h"
 
 
-
+/*
+* brief: This function is used to initialize the necessary drivers for the EEPROM
+*/
 
 void EEPROM_u8Init(){
 	I2C_Init();
 }
-
+/*
+* brief: This function is used to write a byte to one of the connected EEPROMs to the ECU in a specific address
+* param.: (input) the chip to be selected
+* param.: (input) the address to write the byte to 
+* return: (output) the data to be written
+*/
 uint8_t EEPROM_u8WriteByte(uint8_t chip_select, uint16_t ByteAddress, uint8_t data){
 	uint8_t u8Status = I2C_OK;
 	uint8_t device_Address = EEPROM_ADDRESS_MASK | (chip_select);
@@ -34,7 +41,12 @@ uint8_t EEPROM_u8WriteByte(uint8_t chip_select, uint16_t ByteAddress, uint8_t da
 	I2C_Stop();
 	return u8Status;
 }
-
+/*
+* brief: This function is used to read a byte from one of the connected EEPROMs to the ECU from a specific address
+* param.: (input) the chip to be selected
+* param.: (input) the address to read the byte from
+* return: (output) a pointer to a variable to hold the data that will be read
+*/
 uint8_t EEPROM_u8ReadByte(uint8_t chip_select, uint16_t ByteAddress, uint8_t* data){
 	uint8_t u8Status = I2C_OK;
 	uint8_t device_Address = EEPROM_ADDRESS_MASK | (chip_select);
@@ -56,7 +68,12 @@ uint8_t EEPROM_u8ReadByte(uint8_t chip_select, uint16_t ByteAddress, uint8_t* da
 	I2C_Stop();
 	return u8Status;
 }
-
+/*
+* brief: This function is used to write a string to one of the connected EEPROMs to the ECU in a specific address
+* param.: (input) the chip to be selected
+* param.: (input) the address to start writing the string in
+* return: (output) a pointer to the string that will be written
+*/
 uint8_t EEPROM_u8WriteString(uint8_t chip_select, uint16_t stringAddress, uint8_t* str){
 	uint8_t u8Status = I2C_OK;
 	uint8_t device_Address = EEPROM_ADDRESS_MASK | (chip_select);
@@ -77,7 +94,12 @@ uint8_t EEPROM_u8WriteString(uint8_t chip_select, uint16_t stringAddress, uint8_
 	I2C_Stop();
 	return u8Status;
 }
-
+/*
+* brief: This function is used to read a string from one of the connected EEPROMs to the ECU starting from a specific address
+* param.: (input) the chip to be selected
+* param.: (input) the address to start reading the string from
+* return: (output) a pointer to hold the string that will be read
+*/
 uint8_t EEPROM_u8ReadString(uint8_t chip_select, uint16_t stringAddress, uint8_t* str, uint8_t str_size){
 	uint8_t u8Status = I2C_OK;
 	uint8_t device_Address = EEPROM_ADDRESS_MASK | (chip_select);
